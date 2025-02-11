14d5da34928f3f9212275ff3f7e86e5e2dd29741
Caleb Sebsibe <calebsebsibe.custor@gmail.com>
2025-02-11 14:43:56 +0300

    Added to Petlist

--- src/app/Components/pet-list/pet-list.component.ts
+++ src/app/Components/pet-list/pet-list.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import { MatLabel } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule} from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
+interface Food {
+  value: string;
+  viewValue: string;
+}
 
 @Component({
   selector: 'app-pet-list',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, MatLabel, MatButtonModule, MatCardModule, MatChipsModule, MatSelectModule, MatPaginatorModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './pet-list.component.html',
   styleUrl: './pet-list.component.css'
 })
 export class PetListComponent {
-
+  foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'},
+  ];
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { PetListComponent } from "./Components/pet-list/pet-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, PetListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
