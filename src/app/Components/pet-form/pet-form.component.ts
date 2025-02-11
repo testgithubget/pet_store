397d9b8b61ae8f933cf6bbcfe7004afee0ccb8a8
GetyeDg <degelogetye@gmail.com>
2025-02-11 09:17:18 +0300

    added new feature on develop

---
+++ src/app/Components/pet-form/pet-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pet-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './pet-form.component.html',
+  styleUrl: './pet-form.component.css'
+})
+export class PetFormComponent {
+
+}
