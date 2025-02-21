1a880e447cfb14966893dad26326cff17dfe4779
Caleb <calebsebsibe.custor@gmail.com>
2025-02-21 16:01:00 +0300

    Fixed Menu

--- src/app/Components/menu/menu.component.ts
+++ src/app/Components/menu/menu.component.ts
@@ -1,8 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {MatIconModule} from '@angular/material/icon';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
-import { RouterModule } from '@angular/router';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -11,6 +11,26 @@ import { RouterModule } from '@angular/router';
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  isOnAdd: boolean = false;
+  isOnList: boolean = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/add') {
+          this.isOnAdd = true;
+          this.isOnList = false;
+        } else if (event.url === '/list') {
+          this.isOnAdd = false;
+          this.isOnList = true;
+        }else{
+          this.isOnAdd = true;
+          this.isOnList = true;
+        }
+      }
+    });
+  }
 }
